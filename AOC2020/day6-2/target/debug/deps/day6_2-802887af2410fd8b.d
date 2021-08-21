a0067708d0769e7ebf56733bdc1e20bbcb00a8b0
Nima155 <nimashadab@hotmail.co.uk>
2021-08-21 22:12:17 +0430

    .

---
+++ AOC2020/day6-2/target/debug/deps/day6_2-802887af2410fd8b.d
@@ -0,0 +1,5 @@
+c:\Users\Hot-Apple\Advent Of Code\AOC2020\day6-2\target\debug\deps\day6_2-802887af2410fd8b.rmeta: src\main.rs
+
+c:\Users\Hot-Apple\Advent Of Code\AOC2020\day6-2\target\debug\deps\day6_2-802887af2410fd8b.d: src\main.rs
+
+src\main.rs:
---
+++ AOC2020/day6/target/debug/deps/day6-cfbc9de28a91ec88.d
@@ -0,0 +1,7 @@
+C:\Users\Hot-Apple\Advent Of Code\AOC2020\day6\target\debug\deps\day6-cfbc9de28a91ec88.rmeta: src\main.rs
+
+C:\Users\Hot-Apple\Advent Of Code\AOC2020\day6\target\debug\deps\day6-cfbc9de28a91ec88.d: src\main.rs
+
+src\main.rs:
+
+# env-dep:CLIPPY_ARGS=
