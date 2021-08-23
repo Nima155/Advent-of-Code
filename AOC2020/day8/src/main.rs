e340878fc5fc0f05fec7464fef7deff0da4fc2d4
Nima155 <nimashadab@hotmail.co.uk>
2021-08-23 16:03:35 +0430

    day8-1

---
+++ AOC2020/day8/src/main.rs
@@ -0,0 +1,36 @@
+use std::{collections, fs};
+
+fn main() {
+    let read = fs::read_to_string("input.txt").unwrap();
+
+    let vec = read
+        .split("\r\n")
+        .map(|instruct| instruct.split(' ').collect())
+        .collect::<Vec<Vec<_>>>();
+
+    let mut acc = 0;
+    let mut cur_indx: i32 = 0;
+
+    let mut visited = collections::HashSet::new();
+    loop {
+        let instruction = &vec[cur_indx as usize];
+
+        if visited.contains(&cur_indx) {
+            break println!("{}", acc);
+        }
+
+        visited.insert(cur_indx);
+
+        match instruction[0] {
+            "acc" => {
+                acc += instruction[1].parse::<i32>().unwrap();
+                cur_indx += 1
+            }
+            "jmp" => cur_indx += instruction[1].parse::<i32>().unwrap(),
+            "nop" => {
+                cur_indx += 1;
+            }
+            _ => {}
+        }
+    }
+}
