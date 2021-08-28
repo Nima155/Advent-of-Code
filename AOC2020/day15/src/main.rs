39dd8e3519755b3254ad2bb5bb1e6d32a3ca5707
Nima155 <nimashadab@hotmail.co.uk>
2021-08-28 20:26:24 +0430

    day15-1

---
+++ AOC2020/day15/src/main.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+fn main() {
+    let input = [1,20,11,6,12,0];
+
+    let mut to_map: HashMap<i32, usize> = input.iter().enumerate().map(|(i,e)| (*e, i + 1)).collect();
+
+    let (mut turn, mut last) = (input.len() + 1, 0);
+    
+
+    while turn < 2020 {
+        
+        match to_map.get_mut(&last) {
+            Some(v) => { last = (turn - *v) as i32; *v = turn; },
+            None => { to_map.insert(last, turn); last = 0; }
+        }
+        turn += 1;
+    }
+    println!("{}", last);
+}
