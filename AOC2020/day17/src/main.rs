e726aff8fd18f1503536406f947fc667b4fb4ef8
Nima155 <nimashadab@hotmail.co.uk>
2021-08-30 23:31:19 +0430

    day17-1

---
+++ AOC2020/day17/src/main.rs
@@ -0,0 +1,28 @@
+use dimensional_ops::{get_active_nodes, node_cycle};
+
+
+
+mod dimensional_ops;
+
+fn main() {
+    
+
+    let input = "#......#\n##.#..#.\n#.#.###.\n.##.....\n.##.#...\n##.#....\n#####.#.\n##.#.###"
+        .split('\n');
+
+    let _mock_input = ".#.\n..#\n###".split('\n');
+
+    let grid = input.map(|e| e.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+
+    let mut actives = get_active_nodes(&grid);
+
+    let mut cycles = 0;
+
+    while cycles < 6 {
+
+        actives = node_cycle(&actives);
+        cycles += 1;
+
+    }
+    println!("{}", actives.len());
+}
