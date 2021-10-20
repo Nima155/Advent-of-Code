6e6f4fb0ac87d7b45bcdb906a3651c0e95e71e51
Nima155 <nimashadab@hotmail.co.uk>
2021-10-20 15:41:27 +0330

    completed day14

--- AOC2018/day14/day14-2/src/main.rs
+++ AOC2018/day14/day14-2/src/main.rs
@@ -4,15 +4,19 @@ fn main() {
     let (mut minion_1, mut minion_2) = (0, 1);
 
 
-    while initial.len() < 409561 {
+    'a: loop {
         let new_recipe_parts =  (initial[minion_1] + initial[minion_2]).to_string();
-
+        
         for dig in new_recipe_parts.chars() {
             initial.push(dig.to_digit(10).unwrap() as usize);
+            if initial.len() >= 6 && initial[initial.len() - 6..] == [4,0,9,5,5,1] {
+                println!("{}", initial.len() - 6);
+                break 'a;
+            }
         }
         minion_1 = (minion_1 + initial[minion_1]  + 1) % initial.len();
         minion_2 = (minion_2 + initial[minion_2] + 1)  % initial.len();
-        
+
     }
-    println!("{:?}", &initial[409551..]);
+    // println!("{:?}", &initial[409551..]);
 }
