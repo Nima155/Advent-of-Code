40c19d698703400dcecf1ce167e00d36aaf126bd
Nima155 <nimashadab@hotmail.co.uk>
2021-11-07 15:14:11 +0330

    completed day03

--- AOC2017/day03/day03-2/main.c
+++ AOC2017/day03/day03-2/main.c
@@ -7,86 +7,101 @@
 20   7   8   9  10
 21  22  23---> ...
 */
-
-// Cantor(x, y) = 1/2 * (x + y) * (x + y + 1) + y
-
 #define INPUT 277678
+#define MAX_X 100000
+#define MAX_Y 1000
 
+int moves[8][2] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1} };
 
-int cantor_hash(int i, int j) {
+int sum_of_neighbors(int i, int j, int *grid) {
+    int sums = 0;
+    for (int m = 0; m < 8; m++) {
+        int y = moves[m][0] + i, x = moves[m][1] + j;
+
+        sums += grid[y * MAX_X + x];
+    }
+    return sums;
 }
 
-void logic(int *i, int *j, int odd_num, int current) {
-    if (current == INPUT) {
-        return;
-    }
+int logic(int i, int j, int odd_num, int *grid) {
 
     int steps = 0;
-    while (steps < odd_num) {
-        *j -= 1;
-        current--;
+    while (steps < (odd_num - 2)) {
+        grid[i * MAX_X + j] = sum_of_neighbors(i, j, grid);
 
-        if (current == INPUT) {
-            return;
+        if (grid[i * MAX_X + j] > INPUT) {
+            return grid[i * MAX_X + j];
         }
-        steps++;
-    }
-    steps = 0;
-
-
-
-    while (steps < odd_num) {
-        *i -= 1;
-        current--;
-
-        if (current == INPUT) {
-            return;
-        }
+        i -= 1;
         steps++;
     }
 
     steps = 0;
 
     while (steps < odd_num) {
-        *j += 1;
-        current--;
+        grid[i * MAX_X + j] = sum_of_neighbors(i, j, grid);
 
-        if (current == INPUT) {
-            return;
+        if (grid[i * MAX_X + j] > INPUT) {
+            return grid[i * MAX_X + j];
         }
+        j -= 1;
         steps++;
     }
+    j += 1;
+    steps = 0;
+
+    while (steps < odd_num) {
+        grid[i * MAX_X + j] = sum_of_neighbors(i, j, grid);
+
+        if (grid[i * MAX_X + j] > INPUT) {
+            return grid[i * MAX_X + j];
+        }
+        i += 1;
+        steps++;
+    }
+    i -= 1;
     steps = 0;
 
 
 
     while (steps < odd_num) {
-        *i += 1;
-        current--;
+        grid[i * MAX_X + j] = sum_of_neighbors(i, j, grid);
 
-        if (current == INPUT) {
-            return;
+        if (grid[i * MAX_X + j] > INPUT) {
+            return grid[i * MAX_X + j];
         }
+        j += 1;
         steps++;
     }
-    steps = 0;
+
+    return 0;
 
 }
 int main()
 {
 
-    int odds = 1, i = 0, j = 0;
+    int odds = 3, i = 500, j = 50000;
 
-    while (1) {
-        if (odds * odds >= INPUT) {
-            logic(&i, &j, odds, odds * odds);
-            printf("answer is: %d", i + j);
-            break;
-        }
-        i++, j++;
-        odds += 2;
+    int *grid = malloc(sizeof(int) * MAX_Y * MAX_X);
 
+    for (int v = 0; v < MAX_X * MAX_Y; v++) {
+            grid[v] = 0;
     }
 
+    grid[i * MAX_X + j] = 1;
+    j++;
+
+    while (1) {
+        int tmp = logic(i, j, odds, grid);
+
+        if (tmp != 0) {
+            printf("%d", tmp);
+            break;
+        }
+        i++;
+        j++;
+        odds += 2;
+    }
+    free(grid);
     return 0;
 }
