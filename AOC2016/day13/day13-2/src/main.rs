702e852daae601bd6876121e5b2458ed7d729ea1
Nima155 <nimashadab@hotmail.co.uk>
2021-11-22 11:36:03 +0330

    completed day13

---
+++ AOC2016/day13/day13-2/src/main.rs
@@ -0,0 +1,41 @@
+use std::collections::{HashSet, VecDeque};
+
+const FAVORITE_NUMBER: i64 = 1364;
+const MOVES: [[i64; 2]; 4] = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+fn main() {
+    // 31, 39
+    println!("{}", count_of_50())
+}
+
+fn is_wall(x: i64, y: i64) -> bool {
+    let mut number = x * x + 3 * x + 2 * x * y + y + y * y + FAVORITE_NUMBER;
+    let mut set_bits = 0;
+    while number != 0 {
+        number &= number - 1;
+        set_bits += 1;
+    }
+    set_bits % 2 != 0
+}
+fn count_of_50() -> usize {
+    let mut vecdeck = VecDeque::new();
+    let mut visited = HashSet::new();
+    vecdeck.push_back((1, 1, 0));
+    visited.insert((1, 1));
+    // 130 is too high
+    let mut ans = 0;
+    while !vecdeck.is_empty() {
+        let (y, x, steps) = vecdeck.pop_front().unwrap();
+        if steps > 50 {
+            continue;
+        }
+        for [yy, xx] in MOVES {
+            let (ny, nx) = (yy + y, x + xx);
+            if ny >= 0 && nx >= 0 && !visited.contains(&(ny, nx)) && !is_wall(nx, ny) {
+                visited.insert((ny, nx));
+                vecdeck.push_back((ny, nx, steps + 1));
+            }
+        }
+        ans += 1;
+    }
+    ans
+}
