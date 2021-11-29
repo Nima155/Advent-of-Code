4bfa54a4e299cf30209794870d6cb8b7e6cc0ff7
Nima155 <nimashadab@hotmail.co.uk>
2021-11-29 11:06:39 +0330

    completed day23

---
+++ AOC2016/day23/day23-2/src/main.rs
@@ -0,0 +1,122 @@
+use std::fs;
+// we modify our input to this: \" is used for padding
+
+// cpy a b
+// dec b
+// cpy a d
+// cpy 0 a
+// "
+// "
+// "
+// "
+// "
+// mul b d
+// dec b
+// cpy b c
+// cpy c d
+// dec d
+// inc c
+// jnz d -2
+// tgl c
+// cpy -16 c
+// jnz 1 c
+// cpy 80 c
+// jnz 97 d
+// inc a
+// inc d
+// jnz d -2
+// inc c
+// jnz c -5
+
+fn main() {
+    let instructions = fs::read_to_string("./input.txt").unwrap();
+    let mut instructions = instructions
+        .split("\r\n")
+        .map(|e| e.split(' ').collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    println!("{}", execute_program(&mut instructions));
+}
+
+fn get_indx(num_in_str: &str) -> usize {
+    (num_in_str.chars().next().unwrap() as u8 - b'a') as usize
+}
+
+const EGGS: i64 = 12;
+fn execute_program(instructions: &mut [Vec<&str>]) -> i64 {
+    let mut registers = [EGGS, 0, 0, 0];
+    let mut i = 0_i64;
+    while i >= 0 && i < instructions.len() as i64 {
+        let split_up = instructions[i as usize].clone();
+        // println!("instruction: {:?} registers: {:?}", split_up, registers);
+        match split_up[0] {
+            "mul" => {
+                let fr;
+                if let Ok(first) = split_up[1].parse::<i64>() {
+                    fr = first;
+                } else {
+                    fr = registers[get_indx(split_up[1])];
+                }
+
+                registers[0] = fr * registers[get_indx(split_up[2])];
+            }
+            "cpy" => {
+                if let Ok(iz) = split_up[1].parse::<i64>() {
+                    registers[get_indx(split_up[2])] = iz;
+                } else if split_up[2].parse::<usize>().is_err() {
+                    registers[get_indx(split_up[2])] = registers[get_indx(split_up[1])];
+                }
+            }
+            "inc" | "dec" => {
+                registers[get_indx(split_up[1])] += if split_up[0] == "inc" { 1 } else { -1 };
+            }
+
+            "jnz" => {
+                let x;
+                if let Ok(iz) = split_up[1].parse::<i64>() {
+                    x = iz;
+                } else {
+                    x = registers[get_indx(split_up[1])];
+                }
+                if x != 0 {
+                    if split_up[2].parse::<i64>().is_ok() {
+                        i += split_up[2].parse::<i64>().unwrap();
+                        continue;
+                    }
+                    i += registers[get_indx(split_up[2])];
+                    continue;
+                }
+            }
+            "tgl" => {
+                let mut new_indx = get_indx(split_up[1]) as i64;
+                new_indx = registers[new_indx as usize] + i as i64;
+                if new_indx < instructions.len() as i64 && new_indx >= 0 {
+                    let point_at_instruction = &mut instructions[new_indx as usize];
+                    match point_at_instruction.len() {
+                        2 => {
+                            point_at_instruction[0] = if point_at_instruction[0] == "inc" {
+                                "dec"
+                            } else {
+                                "inc"
+                            }
+                        }
+                        3 => {
+                            if point_at_instruction[0] != "mul" {
+                                point_at_instruction[0] = if point_at_instruction[0] == "jnz" {
+                                    "cpy"
+                                } else {
+                                    "jnz"
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
+        i += 1;
+    }
+    println!("registers: {:?}", registers);
+    registers[0]
+}
