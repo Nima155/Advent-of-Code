6a630447794eed92eef12f35486e815899d04570
Nima155 <nimashadab@hotmail.co.uk>
2021-12-01 15:45:30 +0330

    completed day12

--- AOC2015/day12/day12-2.py
+++ AOC2015/day12/day12-2.py
@@ -1,19 +1,25 @@
-import re
-with open("input.txt", "r") as input_:
-    input_ = re.sub(r"red", "000000", input_.read())
-    important_matches = re.findall(r"\{|[-]?\d+|\}|\[|\]", input_)
-    bracks, squares, red_alert = [], [], False
-    for mt in important_matches:
-        if mt == "{":
-            bracks.append("{")
-        elif mt == "[":
-            squares.append("[")
-        elif mt == "}":
-            bracks.pop()
-        elif mt == "]":
-            squares.pop()
-        red_alert = mt.isdigit() and mt == "000000"
-        
+import json
 
-    # print(sum(map(int, re.findall("[-]?\d+", input_.read()))))
+def recursive_dive(maps, is_list):
+    tots = 0
     
+    for v in maps:
+        if v == "red" and not is_list:
+            return 0
+        if isinstance(v, dict):           
+            tots += recursive_dive(v.values(), False)
+        elif isinstance(v, list):
+            tots += recursive_dive(v, True)
+        elif isinstance(v, int):
+            tots += v
+    return tots
+
+with open("input.txt", "r") as input_:
+    important_matches = json.load(input_)  
+    tots = 0
+    if isinstance(important_matches, list):
+        tots = recursive_dive(important_matches, True)
+    else:
+        tots = recursive_dive(important_matches.values(), False)
+
+    print(tots)
---
+++ AOC2015/day12/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+if isinstance(important_matches, list):
+    #     for v in important_matches:
+    #         if v == "red":
+    #             break
+    #         if isinstance(v, dict):           
+    #             tots += recursive_dive(v)
+    #         elif isinstance(v, list):
+    #             tots += sum(z if isinstance(z, int) else 0  for z in v)
+    #         elif isinstance(v, int):
+    #             tots += v
+    
