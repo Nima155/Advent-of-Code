c18222440e2845e997feda14240328e62df3781d
Nima155 <nimashadab@hotmail.co.uk>
2021-12-02 11:28:34 +0330

    completed day14

--- AOC2015/day14/day14-2.py
+++ AOC2015/day14/day14-2.py
@@ -1,32 +1,37 @@
 import re
 
-LAST_SECOND = 1000
+LAST_SECOND = 2503
 def parser(text):
 
     return list(map(int, re.findall(r"(?<=fly )\d+|\d+(?= seconds)", text.strip())))
 
-with open("test.txt", "r") as input_:
+with open("input.txt", "r") as input_:
     reindeer = list(map(parser, input_))
     cur_states = {}
+    resevoir = [0] * len(reindeer)
     for s in range(1, LAST_SECOND + 1):
         mx = -1
-        for speed, time, t_t_rest in reindeer:
-            
+        for i, (speed, time, t_t_rest) in enumerate(reindeer):
             rem = min(LAST_SECOND - s, time)
-            
             rein = (speed, time, t_t_rest)
             if rein in cur_states and s == cur_states[rein][1]:
-                cur_states[rein][0] += speed * rem
+                resevoir[i] += rem - 1
+                if resevoir[i] > 0:
+                    cur_states[rein][0] += speed
                 cur_states[rein][1] = rem + s + t_t_rest
-                mx = max(mx, cur_states[rein][0])
             elif rein not in cur_states: 
-                cur_states[rein] = [speed * rem, rem + s + t_t_rest, 0]
-                mx = max(mx, cur_states[rein][0])
+                resevoir[i] = rem - 1
+                cur_states[rein] = [speed, rem + s + t_t_rest, 0]
+            else:
+                if resevoir[i] > 0:
+                    cur_states[rein][0] += speed
+                    resevoir[i] -= 1    
 
-                
-        # if mx != -1:
-            cur_states = { k: (v if v[0] != mx else [v[0], v[1], v[2] + 1]) for k, v in cur_states.items() }
-        
+            mx = max(mx, cur_states[rein][0])
+    
+        cur_states = { k: (v if v[0] != mx else [v[0], v[1], v[2] + 1]) for k, v in cur_states.items() }
+        # print(cur_states)
+    # print(cur_states)
     print(max(map(lambda x: x[2], cur_states.values())))
 
     
