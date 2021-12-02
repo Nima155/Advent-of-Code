e861bda50fea9d7263676330af323251c3ec4b34
Nima155 <nimashadab@hotmail.co.uk>
2021-12-03 01:32:59 +0330

    completed day18

---
+++ AOC2015/day18/day18-2.py
@@ -0,0 +1,28 @@
+from copy import deepcopy
+with open("input.txt", "r") as input_:
+    ls = list(map(lambda x: list(x.strip()), input_.readlines()))
+    ls[0][99] = ls[99][99] = ls[99][0] = ls[0][0] = "#"
+    # diagonals
+    for i in range(100): # steps
+        light_copy = deepcopy(ls)
+        for y in range(100):
+            for x in range(100):
+                light_status = ls[y][x]
+                ons = 0
+                for j in range(-1, 2):
+                    for v in range(-1, 2):
+                        if j != 0 or v != 0:
+                            ny, nx = y + j, x + v
+                            if ny >= 0 and ny < 100 and nx >= 0 and nx < 100:
+                                ons += ls[ny][nx] == "#"
+                if (y, x) not in { (0, 0), (0, 99), (99, 0), (99, 99) }:
+                    if light_status == "#" and ons not in [2, 3]:
+                        light_copy[y][x] = "."
+                    elif light_status == "." and ons == 3:
+                        light_copy[y][x] = "#"
+        ls = light_copy
+    print(sum(sum(map(lambda x: x == '#', v)) for v in ls ))
+# quarantine plan.. vaccine card
+# not staying with daughter 
+
+                            
