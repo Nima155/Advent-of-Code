6c875d64bf3a6243069f3eaa6a39f1f5526d49ed
Nima155 <nimashadab@hotmail.co.uk>
2021-12-05 10:40:56 +0330

    day05 -- better version

--- AOC2021/day05/day05-2/src/main.rs
+++ AOC2021/day05/day05-2/src/main.rs
@@ -17,75 +17,38 @@ fn main() {
     print!("{}", intersection_counter(&points));
 }
 
+
 fn intersection_counter(points: &[Vec<Vec<i64>>]) -> usize {
     let mut visited_counts: HashMap<(i64, i64), i64> = HashMap::new();
     for two_points in points {
-        let [x_eq, y_eq] = [
-            two_points[0][0] == two_points[1][0],
-            two_points[0][1] == two_points[1][1],
-        ];
-        if x_eq || y_eq {
-            let [mut p_1, p_2] = [
-                two_points[0][if x_eq { 1 } else { 0 }],
-                two_points[1][if x_eq { 1 } else { 0 }],
-            ];
-            let stable_point = if x_eq {
-                two_points[0][0]
-            } else {
-                two_points[0][1]
-            };
-            loop {
-                
-                let ent = visited_counts
-                    .entry((
-                        if x_eq { stable_point } else { p_1 },
-                        if y_eq { stable_point } else { p_1 },
-                    ))
-                    .or_default();
-                *ent += 1;
-
-                if p_1 == p_2 {
-                    break;
-                }
-                if p_1 > p_2 {
-                    p_1 -= 1;
-                } else {
-                    p_1 += 1;
-                }
-
-                
-            }
-            
-        } else {
-            let [mut p1, mut p2] = [two_points[0][0], two_points[0][1]];
-            loop {
-                
-                let ent = visited_counts
-                    .entry((
-                        p1,
-                        p2
-                    ))
-                    .or_default();
-                *ent += 1;
-
-                if [p1, p2] == two_points[1][..] {
-                    break;
-                }
-                if p1 > two_points[1][0] {
-                    p1 -= 1;
-                } else {
-                    p1 += 1;
-                }
-                if p2 > two_points[1][1] {
-                    p2 -= 1;
-                } else {    
-                    p2 += 1;
-                }
-
-                
-            }
-
-        }
+        map_the_points((two_points[0][0], two_points[0][1]), (two_points[1][0], two_points[1][1] ), &mut visited_counts);
     }
     visited_counts.iter().filter(|(_, v)| **v >= 2).count()
+}
+
+fn map_the_points(mut source: (i64, i64), dest: (i64, i64), visited_counts: &mut HashMap<(i64, i64), i64>) {
+
+    loop {
+        let ent = visited_counts
+                    .entry((
+                        source.0, source.1
+                    ))
+                    .or_default();
+        *ent += 1;
+
+        if source == dest {
+            break;
+        }
+
+        if source.0 > dest.0 {
+            source.0 -= 1;
+        } else if source.0 != dest.0 {
+            source.0 += 1;
+        }
+        if source.1 > dest.1 {
+            source.1 -= 1;
+        } else if source.1 != dest.1 {    
+            source.1 += 1;
+        }
+    }
 }
