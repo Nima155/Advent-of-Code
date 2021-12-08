531bbdf7ec34598efe0504dc68e8ce77dff9c633
Nima155 <nimashadab@hotmail.co.uk>
2021-12-08 14:57:27 +0330

    completed day24 -- the solution could be improved in a number of ways.. one example would be to set lookups but....

--- AOC2015/day24/day24-2.py
+++ AOC2015/day24/day24-2.py
@@ -4,7 +4,7 @@ from itertools import combinations
 INPUT = [1,3,5,11,13,17,19,23,29,31,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]
 arrange = []
 
-TOTAL_SUM_DIV_3 = sum(INPUT) // 3
+TOTAL_SUM_DIV_3 = sum(INPUT) // 4
 
 
 
@@ -15,12 +15,16 @@ def find_optimal_arrangement():
     while i <= len(INPUT) and not seen:
         for group in combinations(INPUT, i):
             if sum(group) == TOTAL_SUM_DIV_3:
-                # print(group)
                 for j in range(1, (len(INPUT) - len(group)) + 1):
                     for group_sec in combinations([v for v in INPUT if v not in group], j):
                         if sum(group_sec) == TOTAL_SUM_DIV_3:
-                            if sum(v for v in INPUT if v not in group and v not in group_sec) == TOTAL_SUM_DIV_3:
-                                return reduce(mul, group)
+                            for x in range(1, len(INPUT) - len(group) - len(group_sec) + 1):
+                                for group_third in combinations([v for v in INPUT if v 
+                                                                not in group_sec and v not in group], x):
+                                    if sum([v for v in INPUT if v not in group and 
+                                                                v not in group_sec and v not in group_third]) == TOTAL_SUM_DIV_3:
+                                        return reduce(mul, group)
+                                
                 
         i += 1
     
