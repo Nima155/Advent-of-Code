d14e90617227a1d92f50b89066cd70de16bef47e
Nima155 <nimashadab@hotmail.co.uk>
2021-12-08 14:52:40 +0330

    day24-1

--- AOC2015/day24/day24.py
+++ AOC2015/day24/day24.py
@@ -1,4 +1,6 @@
-from functools import lru_cache
+from operator import mul
+from functools import lru_cache, reduce
+from itertools import combinations
 from collections import deque
 from copy import deepcopy
 INPUT = [1,3,5,11,13,17,19,23,29,31,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]
@@ -9,24 +11,21 @@ TOTAL_SUM_DIV_3 = sum(INPUT) // 3
 
 
 def find_optimal_arrangement():
-    queue = deque([[[0, 0, 1], [0, 0, 1], [0, 0, 1]]])
+    i = 1
+    qe = float("inf")
+    seen = False
+    while i <= len(INPUT) and not seen:
+        for group in combinations(INPUT, i):
+            if sum(group) == TOTAL_SUM_DIV_3:
+                # print(group)
+                for j in range(1, (len(INPUT) - len(group)) + 1):
+                    for group_sec in combinations([v for v in INPUT if v not in group], j):
+                        if sum(group_sec) == TOTAL_SUM_DIV_3:
+                            if sum(v for v in INPUT if v not in group and v not in group_sec) == TOTAL_SUM_DIV_3:
+                                return reduce(mul, group)
+                
+        i += 1
     
-    while INPUT:
-        n = INPUT.pop()    
-        n_queue = deque()
-        while queue:
-            groups = queue.popleft()
-            for i in range(3):
-                if groups[i][0] + n <= TOTAL_SUM_DIV_3:
-                    groupz = deepcopy(groups)
-                    groupz[i][0] += n
-                    groupz[i][1] += 1
-                    groupz[i][2] *= n
-                    n_queue.append(groupz)
-        queue = n_queue
-        print(len(INPUT))
-    print(sorted(queue))
-
 
 
 
